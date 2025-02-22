d250e71fa5b2e64465ba1c17fef12c2812884a2d
talz-a <talal.gl2004@gmail.com>
2025-02-22 13:55:23 -0500

    added in cors policy

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,25 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from llm import getPolicies
+
+
+load_dotenv()
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/swipe")
 def getPolicy():
     return getPolicies()
+
 
 @app.get("/")
 async def root():
