4eab9a1e3581da0021eeb8a06417d1c5518996d9
Ray Wang <81761698+R4y1237@users.noreply.github.com>
2025-02-22 11:48:33 -0500

    updated venv and api key input

--- backend/llm.py
+++ backend/llm.py
@@ -1,13 +1,12 @@
 import getpass
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 from pse import get_political_articles
 
-#os.environ.get("OPENAI_API_KEY") = 
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-if not os.environ.get("OPENAI_API_KEY"):
-  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
-  
 
 from langchain.chat_models import init_chat_model
 
